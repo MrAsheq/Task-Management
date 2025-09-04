93d6fb9d4e8bfb62008f5837517db43032a5caee
mrAsh <misterasheq@outlook.com>
2025-09-04 20:48:17 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,15 @@ void main() {
 
 
 
-/// git update code
-///git remote add origin https://github.com/MrAsheq/Task-Management.git
-// git branch -M main
-// git push -u origin main
+/// git update code:
+/// git add .
+// git commit -m "Your commit message"
+// git push origin main
+
+// one line to update
+///   git add . && git commit -m "your commit message" && git push origin main
+
+
 
 /// sdk version showing
 /// fvm- flutter version managemdart daent
