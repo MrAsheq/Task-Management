8ce882644c2feacf1bbc468861ebe13442084240
mrAsh <misterasheq@outlook.com>
2025-09-10 14:39:30 +0600

    Clear themedata

--- lib/app.dart
+++ lib/app.dart
@@ -10,20 +10,32 @@ class TaskManagerApp extends StatelessWidget {
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow,
+          brightness: Brightness.light,
         ),
+
         inputDecorationTheme: InputDecorationTheme(
           hintStyle: TextStyle(color: Colors.grey),
-
           fillColor: Colors.white,
           filled: true,
           contentPadding: EdgeInsets.symmetric(horizontal: 15),
           border: OutlineInputBorder(borderSide: BorderSide.none),
           enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
-          errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          errorBorder: OutlineInputBorder(borderSide: BorderSide.none)
         ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.yellow,
+            foregroundColor: Colors.black
+          )
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.yellow
+          )
+        ),
+        useMaterial3: true
       ),
     );
   }
